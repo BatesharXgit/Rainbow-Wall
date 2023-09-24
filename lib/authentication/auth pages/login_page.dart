002f19b29dc1a73e470f47ee9ac31d379fdacced
BatesharXgit <bateshar.xyz@gmail.com>
2023-09-25 00:11:01 +0530

    login page

--- lib/authentication/auth pages/login_page.dart
+++ lib/authentication/auth pages/login_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -27,94 +29,103 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       backgroundColor: Color(0xFF131321),
       body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              const SizedBox(height: 20),
-              SizedBox(
-                height: 180,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(6),
-                  child: Image.asset('lib/images/luca.png'),
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('lib/images/bg.jpg'),
+                  fit: BoxFit.cover,
                 ),
               ),
-              Center(
-                child: Text(
-                  'LUCA',
-                  style: TextStyle(
-                    fontFamily: 'Anurati',
-                    color: Colors.white,
-                    fontSize: 50,
-                  ),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                child: Container(
+                  color: Colors.black.withOpacity(0.5),
                 ),
               ),
-              const SizedBox(height: 2),
-              Align(
-                alignment: Alignment.center,
-                child: Text(
-                  ('"Step into a World of Wall Artistry: \n       Your Screens, Our Canvas!"'),
-                  style: GoogleFonts.kanit(color: Colors.white, fontSize: 18),
-                ),
-              ),
-              SizedBox(height: MediaQuery.of(context).size.height * 0.1),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  SquareTile(
-                    onTap: () => AuthService().signInWithGoogle(),
-                    imagePath: 'lib/images/google.png',
-                    text: 'Sign In with Google',
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    height: 180,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(6),
+                      child: Image.asset('lib/images/luca.png'),
+                    ),
                   ),
-                ],
-              ),
-              // Row(
-              //   mainAxisAlignment: MainAxisAlignment.center,
-              //   children: [
-              //     SquareTile(
-              //       onTap: () => _signInAnonymously,
-              //       imagePath: 'lib/images/google.png',
-              //       text: 'Sign In Anonymously',
-              //     ),
-              //   ],
-              // ),
-              const SizedBox(
-                height: 10,
-              ),
-              Column(
-                children: [
+                  Center(
+                    child: Text(
+                      'LUCA',
+                      style: TextStyle(
+                        fontFamily: 'Anurati',
+                        color: Colors.white,
+                        fontSize: 50,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 2),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      ('"Step into a World of Wall Artistry: \n       Your Screens, Our Canvas!"'),
+                      style:
+                          GoogleFonts.kanit(color: Colors.white, fontSize: 18),
+                    ),
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.1),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text(
-                        'By Continuing, you agree with Luca',
-                        style: TextStyle(
-                          color: Colors.grey[500],
-                          fontSize: 12,
-                        ),
+                      SquareTile(
+                        onTap: () => AuthService().signInWithGoogle(),
+                        imagePath: 'lib/images/google.png',
+                        text: 'Sign In with Google',
                       ),
-                      const SizedBox(width: 4),
                     ],
-                  ),
-                  GestureDetector(
-                    onTap: () {},
-                    child: Text(
-                      'Privacy Policy',
-                      style: TextStyle(
-                        decoration: TextDecoration.underline,
-                        color: Color(0xFFE6EDFF),
-                        fontWeight: FontWeight.bold,
-                        fontSize: 14,
-                      ),
-                    ),
                   ),
                   const SizedBox(
                     height: 10,
                   ),
+                  Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'By Continuing, you agree with Luca',
+                            style: TextStyle(
+                              color: Colors.grey[500],
+                              fontSize: 12,
+                            ),
+                          ),
+                          const SizedBox(width: 4),
+                        ],
+                      ),
+                      GestureDetector(
+                        onTap: () {},
+                        child: Text(
+                          'Privacy Policy',
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            color: Color(0xFFE6EDFF),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                    ],
+                  )
                 ],
-              )
-            ],
-          ),
+              ),
+            ),
+          ],
         ),
       ),
     );
